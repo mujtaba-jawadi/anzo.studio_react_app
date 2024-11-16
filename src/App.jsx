51ff4aaeb9f64a529f87a08c4ef1f41be14ee3ef
Muhammad Mujtaba Jawadi <mujtabafalcon986@gmail.com>
2024-11-16 08:58:05 +0500

    Setting up react@vite project

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import { Home } from "./pages/Home"
+
+export const App = () => {
+  return (
+    <>
+      <Home/>
+    </>  )
+}
---
+++ src/pages/Home.jsx
@@ -0,0 +1,9 @@
+export const Home = () => {
+  return (
+    <div className="h-screen p-3 bg-white">
+        <div className="shadow-lg shadow-black w-full h-full rounded-[30px] bg-[url(../public/imgs/ANZO.webp)] bg-cover">
+
+        </div>
+    </div>
+  )
+}
